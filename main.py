e6d9cb59091de8cab05fd3423b1ebc3dd594a6b5
fokamo <52177356+fokamo@users.noreply.github.com>
2021-12-26 17:12:23 -0800

    code cleanup & drawing overhaul

--- main.py
+++ main.py
@@ -12,38 +12,36 @@ import end
 import game
 import screen
 
-# required initialization step
 import start
 
+# required initialization step
 pygame.init()
 
 # set up game clock
-FPS = 2
 game_clock = pygame.time.Clock()
-
 
 WINDOW_SIZE = (400, 800)
 window = pygame.display.set_mode(WINDOW_SIZE)
 
-
 def main() -> None:
+    # initial state
     cur_screen = start.Start(window)
     window.fill(colors.BACKGROUND_COLOR)
     latest_score = 0
+    fps = 2
 
     while True:
-        print('click')
+        # handle user input
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print('mousedown')
                 cur_screen.handle_click(event.pos)
-
             elif event.type == pygame.KEYDOWN:
-                print('keydown')
                 cur_screen.handle_key(event.key)
 
+        # react to user input & run background processes
         cur_screen.update()
 
+        # handle reactions to last update
         for event in cur_screen.get_events():
             if event == screen.EXIT_GAME:
                 pygame.quit()
@@ -59,22 +57,18 @@ def main() -> None:
                     cur_screen = game.Game(window)
                 window.fill(colors.BACKGROUND_COLOR)
 
+            elif event == screen.SPEED_UP:
+                fps += 1
+            elif event == screen.RESET_SPEED:
+                fps = 2
+
+        # draw final result
         cur_screen.draw()
 
         # update screen & tick clock
         pygame.display.update()
-        game_clock.tick(FPS)
+        game_clock.tick(fps)
 
 
 if __name__ == '__main__':
     main()
-"""
-
-if __name__ == '__main__':
-    while True:
-        print('click')
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                print('keydown')
-        game_clock.tick(FPS)
-"""
