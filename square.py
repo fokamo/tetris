5bc95069f4716666538c27792ad202637f1d27c2
fokamo <52177356+fokamo@users.noreply.github.com>
2021-12-26 17:14:15 -0800

    code cleanup & drawing overhaul

---
+++ square.py
@@ -0,0 +1,34 @@
+import point
+import pygame
+
+from typing import Tuple
+
+# required initialization step
+pygame.init()
+
+class Square(point.Point):
+    """A square in a game grid. Subclass of Point
+
+    For external use:
+    - .size and .rect for information about the on-screen square
+    - .board_top.left for where the top left of the grid is
+    """
+
+    def __init__(self, pos: Tuple[float, float], size: int, board_top_left: Tuple[int, int]) -> None:
+        super().__init__(pos)
+        self.board_top_left = board_top_left
+        self.size = size
+        self.rect = pygame.Rect(0, 0, size, size)
+        self._set_rect()
+
+    def _set_rect(self):
+        self.rect.left = self.board_top_left[1] + (self.size * (self.col() - 1))
+        self.rect.top = self.board_top_left[0] + (self.size * (self.row() - 1))
+
+    def _move(self, row_move, col_move) -> None:
+        super()._move(row_move, col_move)
+        self.rect.move_ip(col_move * self.size, row_move * self.size)
+
+    def teleport(self, new_pos: Tuple[float, float]) -> None:
+        super().teleport(new_pos)
+        self._set_rect()
