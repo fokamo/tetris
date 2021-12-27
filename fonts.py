d17772bcc93d63d2faf9692a269212be28f30bcc
fokamo <52177356+fokamo@users.noreply.github.com>
2021-12-26 17:11:40 -0800

    code cleanup & drawing overhaul

--- fonts.py
+++ fonts.py
@@ -1,8 +1,8 @@
-"""fonts.py: a central file of fonts
-Fonts available:
-TITLE_FONT, SUBTITLE_FONT (both inkfree, subtitle is smaller and italicized)
-BUTTON_FONT (calibri)
-PARAGRAPH_FONT, NUMBER_FONT (both arial, number is smaller)
+"""fonts.py - a central file of fonts
+
+For external use:
+- .TITLE_FONT, .SUBTITLE_FONT (both inkfree, subtitle is smaller and italicized)
+- .BUTTON_FONT (calibri)
 """
 
 import pygame
@@ -12,5 +12,4 @@ pygame.init()
 
 TITLE_FONT = pygame.font.SysFont('inkfree', 60)
 SUBTITLE_FONT = pygame.font.SysFont('inkfree', 20, False, True)
-
 BUTTON_FONT = pygame.font.SysFont('calibri', 35)
