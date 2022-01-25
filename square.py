59444aa535a03ca84d3764be9402735d792f2276
fokamo <52177356+fokamo@users.noreply.github.com>
2022-01-25 07:55:53 -0800

    Look-ahead piece bank

--- square.py
+++ square.py
@@ -14,16 +14,16 @@ class Square(point.Point):
     - .board_top.left for where the top left of the grid is
     """
 
-    def __init__(self, pos: Tuple[float, float], size: int, board_top_left: Tuple[int, int]) -> None:
+    def __init__(self, pos: Tuple[float, float], size: int, board_left_top: Tuple[int, int]) -> None:
         super().__init__(pos)
-        self.board_top_left = board_top_left
+        self.board_left_top = board_left_top
         self.size = size
         self.rect = pygame.Rect(0, 0, size, size)
         self._set_rect()
 
     def _set_rect(self):
-        self.rect.left = self.board_top_left[1] + (self.size * (self.col() - 1))
-        self.rect.top = self.board_top_left[0] + (self.size * (self.row() - 1))
+        self.rect.left = self.board_left_top[0] + (self.size * (self.col() - 1))
+        self.rect.top = self.board_left_top[1] + (self.size * (self.row() - 1))
 
     def _move(self, row_move, col_move) -> None:
         super()._move(row_move, col_move)
